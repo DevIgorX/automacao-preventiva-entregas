7470bf3bc298bdd05312b08ff1c3f4b367f3c6be
Tauam Igor <tauamigor@gmail.com>
2025-10-15 12:44:00 -0300

    adicionando javascript

--- app/static/index.js
+++
@@ -1,6 +0,0 @@
-  <script>
-        document.getElementById('analysis-form').addEventListener('submit', function() {
-            document.getElementById('submit-button').style.display = 'none';
-            document.getElementById('loading-button').style.display = 'block';
-        });
-   </script>
