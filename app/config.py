474a11418eed48693fc5fd27134c0655d24f5f18
tauam <tauam.pinheiro@goias.gov.br>
2025-11-11 16:49:25 -0300

    ajustando estrutura para deploy

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv() #Carrega as variáveis do arquivo .env
+
 
 # Pega o caminho absoluto do script atual (main.py)
 caminho_script = os.path.abspath(__file__)
@@ -11,12 +15,14 @@ diretorio_raiz = os.path.dirname(diretorio_app)
 caminho_dados = os.path.join(diretorio_raiz, 'dados')
 arquivo_gitkeep = os.path.abspath(os.path.join(caminho_dados,'.gitkeep'))
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'chave-padrao-insegura') # Valor padrão apenas para evitar erros locais se esquecer o .env
+DEBUG = os.getenv('FLASK_DEBUG', '0') == '1' # Converte string '1' para boolean True
+
 UPLOAD_FOLDER = caminho_dados
 ROOT_DIR = diretorio_raiz
 GITKEEP = arquivo_gitkeep
 
 
-DEBUG = True
 
 
 
--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from app import create_app
+import os
+from waitress import serve
 
 
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Pega a porta do ambiente ou usa 5000 como padrão
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Verifica se estamos em modo de Debug (configurado no seu .env/config.py)
+    # Se DEBUG for True (seu PC), usa o servidor do Flask
+    if app.config.get('DEBUG'):
+        app.run(host='0.0.0.0', port=port)
+    
+    # Se DEBUG for False (Servidor de Produção), usa o Waitress
+    else:
+        print(f"Iniciando servidor de produção (Waitress) na porta {port}...")
+        serve(app, host='0.0.0.0', port=port)
