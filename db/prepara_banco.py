d5fd81956138c69f3b4c7d73569eba9f24cc2ad6
Tauam Igor <tauamigor@gmail.com>
2025-12-14 00:30:06 -0300

    modelando banco de dados

--- db/prepara_banco.py
+++ db/prepara_banco.py
@@ -1,22 +1,28 @@
 import sqlite3
 
 
-conn = sqlite3.connect('automacao.db')
-cursor = conn.cursor()
+conn = sqlite3.connect('automacao.db') #conn para se conectar com o banco
+cursor = conn.cursor() #cursor para executar as querys no banco
 
 tabela_usuario = """
 CREATE TABLE IF NOT EXISTS usuarios(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT,
+    email TEXT,
     senha TEXT
 )
 """
 
 cursor.execute(tabela_usuario)
 
-cursor.execute("INSERT INTO usuarios(nome, senha) values ('Tauam Igor', '1234')")
+cursor.execute("INSERT INTO usuarios(nome, email, senha ) values ('Tauam Igor', 'tauamigor@gmail.com','1234')")
 
 #consultando dados
 
-conn.commit()
-conn.close()
+consulta = """ SELECT * FROM usuarios """
+resultado = cursor.execute(consulta).fetchall()
+print(resultado)
+
+
+conn.commit() #salva a alteração no banco
+conn.close() #fecha a conexão com o banco de dados 
